fff0dbca90363596c6116edeee62ddbadb5fada1
Gabriel Montaño <1245@holbertonschool.com>
2019-09-20 20:22:13 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number = 0;
+for (number == 0; number < 10; number++)
+{
+printf("%d", number);
+}
+printf("\n");
+return (0);
+}
