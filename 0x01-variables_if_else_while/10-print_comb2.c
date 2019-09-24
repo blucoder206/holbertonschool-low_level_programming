acf914eaf02f7a8f1d2c9ee7af71606ffe3528da
Gabriel Montaño <1245@holbertonschool.com>
2019-09-24 22:06:47 +0000

    00..99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - print numbers from 00 to 99 with , and space
+ *Return: 0
+ */
+int main(void)
+{
+	int number;
+	int number2;
+
+	number = 48;
+	number2 = 48;
+	while (number < 58)
+	{
+		while (number2 < 58)
+		{
+			putchar(number);
+			putchar(number2);
+			if (number != 57 ||  number2 != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			number2++;
+		}
+		number2 = 48;
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
