e5a8e1e5bf8caab6fc8d14d1b25ac8e1afe8fede
Gabriel Montaño <1245@holbertonschool.com>
2019-09-24 17:24:38 +0000

    Deleted another way to perform the requested action

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,6 @@
 */
 void print_alphabet(void)
 {
-/** Way 1 **/
-
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
@@ -15,15 +13,4 @@ void print_alphabet(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-/** Way 2**/
-/**
-*	char c[] = "abcdefghijklmnopqrstuvwxyz";
-*	int var;
-*
-*	for (var = 0; var <= 25; var++)
-*	{
-*	_putchar(c[var]);
-*	}
-*	_putchar('\n');
-**/
 }
