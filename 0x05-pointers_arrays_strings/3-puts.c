fc9521e46524147015b71f37f1cf18e9b5bff44d
Gabriel Montaño <1245@holbertonschool.com>
2019-10-01 18:17:35 +0000

    Put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _puts - This funcion will print a string
+* @str: char type pointer
+**/
+
+void _puts(char *str)
+{
+	int counter;
+
+	for (counter = 0; str[counter] != '\0'; counter++)
+	{
+		_putchar(str[counter]);
+	}
+
+	_putchar('\n');
+}
