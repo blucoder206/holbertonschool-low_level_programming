9e25a9d93cd5a407de31e3760e90ed024e0380cf
Gabriel Montaño <1245@holbertonschool.com>
2019-10-01 22:44:08 +0000

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
 /**
-*
-*
-*
+* puts2 - This funcion only will print the str content if is 0, 2, 4 ,6 or 8
+* @str: char-type pointer
 **/
 
 void puts2(char *str)
@@ -10,11 +9,11 @@ void puts2(char *str)
 	int i = 0;
 	while (str[i] != '\0')
 	{
-		i++;
-		if (i == 2 || i == 4 || i == 6 || i == 8)
+		if (i == 0 || i == 2 || i == 4 || i == 6 || i == 8)
 		{
-		_putchar(str[i]);
+			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
 }
