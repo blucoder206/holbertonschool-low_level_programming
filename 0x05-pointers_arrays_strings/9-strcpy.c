f3acfff2b62d66513195dd89a324f16129722daa
blucoder206 <1245@holbertonschool.com>
2019-10-01 23:52:44 -0500

    Add betty coments

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _strcpy - This funcion will copy an pointer to other
+* @dest: destination
+* @src: source
+* Return - will return dest
+**/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0';)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
