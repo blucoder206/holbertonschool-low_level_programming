b0c7f9bcbd9ea68a8ec8f5aa971d9ab087e07e46
Gabriel Montaño <1245@holbertonschool.com>
2019-10-07 16:28:07 +0000

    Betty comments applied

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _isupper - This funcion will check if a letter is upper or not
+* @c: The letter to check
+* Return: 1 if the letter is uppercase, 0 otherwise
+**/
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+	return (1);
+	}
+	return (0);
+}
+
