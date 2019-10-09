f9bcf94bbb085fbefa25b5d0ca769bd22ae57028
Gabriel Montaño <1245@holbertonschool.com>
2019-10-09 00:46:18 +0000

    Betty comments applied

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 /**
-*
-*
-*
+* main - This funcion will print numbers from 0  to 100, but instead
+*	 multiplies of 3 will print 'Fizz' instead multiplies of 5
+*	 will print 'Buzz' and if the number it's multiplies of 4 and
+*	 5 will print FizzBuzz
+* Return: void
 **/
 void main(void)
 {
 	int numbers;
+
 	for (numbers = 1; numbers <= 100; numbers++)
 	{
 		if (((numbers % 3) == 0) && (numbers % 5) == 0)
