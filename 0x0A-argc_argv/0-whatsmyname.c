5b4f02afb33d55934e56b345a429381b0cea065c
Gabriel Montaño <1245@holbertonschool.com>
2019-10-14 22:21:27 +0000

    [Fix] Betty comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+* main - Will print the name of the executable file
+* Return: 0
+**/
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
