d14a24ec23c26f9b757394d64b8ba5b16a754b9d
Gabriel Montaño <1245@holbertonschool.com>
2019-10-16 21:37:59 +0000

    [Update] Various changes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 /**
- * _strlen - check the length of a string
+ * _strlen - Returns the length of a string
  * @s: pointer type char
  * Return: The program will return the number of characters of *s (the length)
  **/
 int _strlen(char *s)
 {
-	int counter = 0;
+	int i = 0;
 
-	while (s[counter] != '\0')
+	while (s[i] != 00)
 	{
-	counter++;
+		i++;
 	}
-return (counter);
+	return (i);
 }
-
