b35a9a6218161d14e72926cb3fb3ae5fd3acc6c6
blucoder206 <1245@holbertonschool.com>
2019-10-20 12:21:47 -0500

    reverse arry

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* reverse_array - this funcion will reverse an array
+* @a: array elements
+* @n: number of elements
+**/
+void reverse_array(int *a, int n)
+{
+	int i, buf;
+
+	for (i = 0; i < n - 1; i++, n--)
+	{
+		buf = a[i];
+		a[i] = a[n - 1];
+		a[n] = buf;
+	}
+}
