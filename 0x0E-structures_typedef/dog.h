ec2f6802fdb35a64fb34af88e01cc35b16dfa8a1
Gabriel Montaño <1245@holbertonschool.com>
2019-10-21 19:47:19 +0000

    Protptype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,15 @@
 * @age: age of dog
 * @owner: owner of dog
 **/
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
