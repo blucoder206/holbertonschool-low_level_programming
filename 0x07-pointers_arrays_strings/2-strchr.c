f9963d9a08e808681797fb33f2837aed87fc9f2f
blucoder206 <1245@holbertonschool.com>
2019-10-21 00:11:32 -0500

    String Search

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* *_strchr - Search an character in a string
+* @s: string
+* @c: character to find
+*
+* Return: s + i
+**/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != 00; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (00);
+}
