fc2e0e33561f881e8355b50f108b7dfc8fe60671
Gabriel Montaño <1245@holbertonschool.com>
2019-10-21 19:43:35 +0000

    return

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,6 +3,8 @@
 /**
 * print_dog - print a struct dog
 * @d: pointer to struct
+*
+* Return: nothing
 **/
 void print_dog(struct dog *d)
 {
@@ -18,14 +20,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	if (d->age == 00)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %f\n", d->age);
-	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
