649750c8432b0c5d4f168cc13cd7727cd7e69dbf
blucoder206 <1245@holbertonschool.com>
2019-10-28 06:03:10 -0500

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,8 +4,60 @@
 *
 *
 **/
+void pchar(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+void pinteger(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+void pfloat(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+void pstring(va_list arg)
+{
+	char *nostring = va_arg(arg, char *);
+
+	if (nostring == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", nostring);
+}
 void print_all(const char * const format, ...)
 {
-	
+	va_list arg;
 
+	int i = 0;
+	int j = 0;
+	char *separator = "";
+
+	td elem[] = {
+			{"c", pchar},
+			{"i", pinteger},
+			{"f", pfloat},
+			{"s", pstring}
+	};
+
+	va_start(arg, format);
+
+	while (format && format [i])
+	{
+		while (elem[j].c)
+		{
+			if (format[i] == *(elem[j].c))
+			{
+				printf("%s", separator);
+				elem[j].f(arg);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(arg);
 }
