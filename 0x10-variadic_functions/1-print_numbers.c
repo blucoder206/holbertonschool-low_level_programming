8e2408c2a9223de990e53d00008f921e76f41be5
blucoder206 <1245@holbertonschool.com>
2019-10-27 22:19:44 -0500

    [Add] Conditonal != NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,15 +14,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(numbers, n);
 
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		printf("%d", va_arg(numbers, int));
+		if (i < n - 1 && separator != NULL)
 		{
-			printf("%d", va_arg(numbers, int));
-			if (i != n - 1)
-			{
-				printf("%s", separator);
-			}
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
