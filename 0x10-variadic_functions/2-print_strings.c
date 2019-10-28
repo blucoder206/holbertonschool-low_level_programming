83718c99d404cb1eb38941cc86480665d6098e51
blucoder206 <1245@holbertonschool.com>
2019-10-27 22:22:47 -0500

    [Add] Missing va_end

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 /**
 * print_strings - prints strings
 * @separator: separator of every string
@@ -14,16 +12,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(string, n);
 
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		printf("%s", va_arg(string, char *));
+		if (i < n - 1 && separator != NULL)
 		{
-			printf("%s", va_arg(string, char *));
-			if (i != n - 1)
-			{
-				printf("%s", separator);
-			}
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
+	va_end(string);
 }
