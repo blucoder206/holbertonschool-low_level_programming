d22bf4403700713498b00a6c5f83ad913238ff7d
blucoder206 <int.five@ya.ru>
2019-11-01 01:10:04 +0000

    [Add] add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,43 +1,52 @@
 #include "lists.h"
-#include <string.h>
+
 /**
-* _strlen - get length of an passed string
-* @s: string
+* _strlen - get length of an string
+* @str: passed string
 *
-* Return: length of an string
+* Return: length of an strings
 **/
-int _strlen(const char *s)
+int _strlen(const char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != 00; i++)
-	{}
+	for (i = 0; str[i] != 00; i++)
+	{
+	}
 	return (i);
 }
 /**
-* add_node - add node at the beginning of a list_t list
-* @head:
-* @str:
+* add_node - add a new node
+* @head: head of nodes
+* @str: string
 *
-* Return: 
+* Return: node
 **/
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *add_node;
-	const char *st = str;
+	list_t *add_new;
 
-	add_node = malloc(sizeof(list_t));
-	if (add_node == NULL)
-	{
+	add_new = malloc(sizeof(list_t));
+	if (add_new == NULL)
 		return (NULL);
-	}
-	if ((str == NULL) || (head == NULL))
+	if (str == NULL)
 	{
-		return (NULL);
-	}
-	add_node->str = strdup(st);
-	add_node->len = _strlen(st);
-	add_node->next = *head;
+		char *p = "(nil)";
 
-	return (add_node);
+		add_new->str = strdup(p);
+		add_new->len = 0;
+		add_new->next = *head;
+		*head = add_new;
+		return (add_new);
+	}
+	else
+	{
+		add_new->str = strdup(str);
+		add_new->len = _strlen(str);
+		add_new->next = *head;
+		*head = add_new;
+		return (add_new);
+	}
+	return (NULL);
+
 }
