cc8a874fa4b01f785d819f9f5e338131ff9b8ca2
blucoder206 <int.five@ya.ru>
2019-11-04 19:56:03 +0000

    [Add] print number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: passed integer
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+
+}
