3081f0a93450778f204ea11e315e745147c698a8
blucoder206 <int.five@ya.ru>
2019-11-05 22:23:55 +0000

    [Add] Guard

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
 /**
@@ -14,3 +16,4 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+#endif
