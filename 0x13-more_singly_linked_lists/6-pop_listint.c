3b6ea7f2136ec7962d9b88a82eebb18c3eae8895
blucoder206 <int.five@ya.ru>
2019-11-06 01:51:53 +0000

    [Add] pop list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* pop_listint - delete the head of node
+* @head: head
+* Return: 0 of list is empty
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *guard;
+	int g;
+
+	if (*head != NULL)
+	{
+		guard = (*head)->next;
+		g = (*head)->n;
+		*head = guard;
+		return (g);
+	}
+	return (0);
+}
