5d81fc364c1f0c05570894fd0ddd681b849f8021
blucoder206 <int.five@ya.ru>
2019-11-06 00:18:25 +0000

    [Add] lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,4 +19,5 @@ typedef struct listint_s
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
